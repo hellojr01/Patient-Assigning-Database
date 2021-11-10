f16e3c2ac5f6dbf9afade4359cf27ed63e29767c
Lee Jia Rou <leejiarou1029@gmail.com>
2021-11-11 01:27:08 +0800

    fix: Update files

--- controller/medicineReportController.java
+++ controller/medicineReportController.java
@@ -6,17 +6,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import java.util.List;
 import java.io.IOException;
 import javafx.scene.Parent;
+import model.Database;
+import javafx.scene.control.Label;
 
 public class medicineReportController {
     
     Stage primaryStage;
     Parent root;
+
+    @FXML
+    private Label address;
+
+    @FXML
+    private Label dob;
+
+    @FXML
+    private Label name;
     
     @FXML
     void refreshInfo(ActionEvent event) {
-        
+        List<List<String>> patient_info = Database.readData("Patient");
+        name.setText(patient_info.get(0).get(0));
+        address.setText(patient_info.get(0).get(9) + "," + patient_info.get(0).get(7) + " " + patient_info.get(0).get(6) + "," + patient_info.get(0).get(8));
+        dob.setText(patient_info.get(0).get(5));
     }
 
     @FXML
